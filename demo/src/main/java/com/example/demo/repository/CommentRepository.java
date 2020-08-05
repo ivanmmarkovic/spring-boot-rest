dd44435420b0ada256bb7203466f27bf8a8b9888
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2020-08-04 18:42:48 -0700

    Add Repository annotation

--- demo/src/main/java/com/example/demo/repository/CommentRepository.java
+++ demo/src/main/java/com/example/demo/repository/CommentRepository.java
@@ -14,6 +14,7 @@ import org.springframework.jdbc.support.KeyHolder;
 
 import com.example.demo.domain.Comment;
 
+
 public class CommentRepository implements CommonRepository<Comment> {
 
 	@Autowired
